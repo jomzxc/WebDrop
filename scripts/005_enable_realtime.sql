d0fd4efabf2ffad3bd230070abab394e5250a894
JOM <74537369+jomzxc@users.noreply.github.com>
2025-11-04 22:15:03 +0800

    Update realtime publication for peers and rooms
    Removed signaling table from realtime publication.

--- scripts/005_enable_realtime.sql
+++ scripts/005_enable_realtime.sql
@@ -1,8 +1,5 @@
 -- Enable realtime for peers table
 alter publication supabase_realtime add table public.peers;
 
--- Enable realtime for signaling table
-alter publication supabase_realtime add table public.signaling;
-
 -- Enable realtime for rooms table
 alter publication supabase_realtime add table public.rooms;
